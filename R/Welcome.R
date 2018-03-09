75be24c0713aca9d44a9a4df1e931e6bdcf8b566
loicverlingue <loicverlingue@yahoo.fr>
2018-03-09 15:53:22 +0100

    Welcome.R
    Preliminary requirements: loading packages, data

---
+++ R/Welcome.R
@@ -0,0 +1,51 @@
+#######
+# welcome to ArtMolNet
+# Artificial Molecular Network building, simulation and optimisation
+# dedicated to anti-cancer treatments
+# Loic Verlingue, MD PhDc, DITEP, U900
+
+# load packages
+library(reshape2)
+library(recount)
+install.packages("glmnet")
+library(glmnet)
+library(stringr)
+
+# instal MaBoSS in the /tmp/ direction
+
+#######
+# create folders
+getwd()
+setwd("~/Bureau/R/")
+dir.create(file.path(getwd(), "model"), showWarnings = FALSE)
+dir.create(file.path(getwd(), "tmp"), showWarnings = FALSE)
+
+########
+# load data net and FamilyGenes
+list.files(paste(getwd(),"/data/",sep = ""))
+OMNI<-read.csv2(paste(getwd(),"/data/OMNI.csv",sep = ""), stringsAsFactors = F)
+ImmunGenes<-read.csv2(paste(getwd(),"/data/ImmunGenes.csv",sep = ""), stringsAsFactors = F)
+ImmunGenes<-unique(ImmunGenes$gene)
+
+#######
+# name your project
+NameProj<-"Immuno"
+
+#######
+# select your genes manually
+# may propose a selection in a list
+GENESman<-c("PDCD1", "CD274", "CTLA4") #"EGFR
+
+#######
+# select the treatment targets manually
+treatmt<-c("CD274", "CTLA4") #"EGFR"
+treatmt<-paste(treatmt,"_TTT",sep = "")
+
+#######
+# define the gene family you want to keep
+ImmunGenes
+
+#### 
+# build NET
+NET<-OMNI[OMNI$source_hgnc%in%union(GENESman,gsub("_TTT","",treatmt))|OMNI$target_hgnc%in%union(GENESman,gsub("_TTT","",treatmt)),]
+# and go to NetBuilding()
