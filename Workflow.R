fe3af39e0dd8601bca975d0ae3f8d858dd487840
loicverlingue <loicverlingue@yahoo.fr>
2018-04-05 15:14:00 +0200

    Update Workflow.R

--- Workflow.R
+++ Workflow.R
@@ -16,6 +16,10 @@ TotAttractors<-BoolSimul(NETall,ASYNC = T, Boolean = F, NumiStates = 100)
 # plot the attractors (in /tmp/attractor.pdf) and write table with best attractors
 BestAttract<-PlotAttractors(TotAttractors,PDF = T)
 
+# plot species activities
+SpeciesActivity<-plotProbaState(Species = union(NETall$source_hgnc,NETall$target_hgnc),
+               TotAttractors = TotAttractors, Stochastic = F) 
+
 
 #######
 # MaBoSS simulation
